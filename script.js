2709e41a6db95ba73e8862dd4871d45d566e600c
mayliviiaa <nurazizahnganjuk2010@gmail.com>
2026-02-22 17:49:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+    // Toggle Nav
+    nav.classList.toggle('nav-active');
+
+    // Burger Animation (Optional)
+    burger.classList.toggle('toggle');
+});
